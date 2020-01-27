baae397f9e3c543f9403929648be7f81caf40554
Adiprasetiyo1 <adi.17411075@student.ubl.ac.id>
2020-01-27 13:52:50 +0700

    first commit

---
+++ app/src/main/java/com/adi/tugas2adi/MainActivity.java
@@ -0,0 +1,44 @@
+package com.adi.tugas2adi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtNpm;
+    EditText edtNama;
+    Button btnSend;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtNpm  = (EditText) findViewById(R.id.edtNpm);
+        edtNama = (EditText) findViewById(R.id.edtNama);
+
+        btnSend = (Button) findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String Npm = edtNpm.getText().toString();
+                String Nama = edtNama.getText().toString();
+
+                Intent next = new Intent(MainActivity.this,Halaman2.class);
+                next.putExtra("txtNpm", Npm);
+                next.putExtra("txtNama", Nama);
+                startActivity(next);
+                finish();
+            }
+        });
+    }
+}
